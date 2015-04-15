f85ddaa68eb32be19c74fceaea33146f850b84aa
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-04-15 12:57:41 -0400

    Fix styling in cookies.js
    Adds missing '{'
    Fixes the indentation

--- public/javascripts/cookies.js
+++ public/javascripts/cookies.js
@@ -1,37 +1,45 @@
 function createCookie(name, value, expires, path, domain) {
-  var cookie = name + "=" + escape(value) + ";";
- 
-  if (expires) {
-    // If it's a date
-    if(expires instanceof Date) {
-      // If it isn't a valid date
-      if (isNaN(expires.getTime()))
-       expires = new Date();
+    var cookie = name + "=" + escape(value) + ";";
+
+    if (expires) {
+        // If it's a date
+        if (expires instanceof Date) {
+            // If it isn't a valid date
+            if (isNaN(expires.getTime())) {
+                expires = new Date();
+            } else {
+                expires = new Date(new Date().getTime() + parseInt(expires) * 1000 * 60 * 60 * 24);
+            }
+        }
+
+        cookie += "expires=" + expires.toGMTString() + ";";
     }
-    else
-      expires = new Date(new Date().getTime() + parseInt(expires) * 1000 * 60 * 60 * 24);
- 
-    cookie += "expires=" + expires.toGMTString() + ";";
-  }
- 
-  if (path)
-    cookie += "path=" + path + ";";
-  if (domain)
-    cookie += "domain=" + domain + ";";
- 
-  document.cookie = cookie;
+
+    if (path) {
+        cookie += "path=" + path + ";";
+        if (domain) {
+            cookie += "domain=" + domain + ";";
+        }
+
+        document.cookie = cookie;
+    }
 }
+
 function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
 }
 
 function eraseCookie(name) {
-	createCookie(name,"",-1);
+    createCookie(name, "", -1);
 }
