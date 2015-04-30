f2be6dfb111ad65ab8a89bf250a084f05ec14188
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-04-30 19:36:58 -0400

    Fix deploy.sh and add specific way for my VPS

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
-#!usr/bin/sh
+#!/usr/bin/sh
+# Comment these out or change them if you're not using this on wolfy1339's VPS
+SCREEN1_ID=12509
+SCREEN2_ID=12491
+
 git pull
+
 #Run Node servers in background
-node app.js &
-node app_static.js
+if [-z $SCREEN1_ID] && [-z $SCREEN2_ID]
+    then
+        screen -r  $SCREEN1_ID -X stuff 'node app.js\n'
+        screen -r $SCREEN2_ID -X stuff 'node app_static.js\n'
+    else
+        node app.js &
+        node app_static.js &
+fi
