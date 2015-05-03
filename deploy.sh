d35b654b5109a206a0b6a5069e5599eeb048fdb6
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-05-03 11:46:51 -0400

    Fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 # Comment these out or change them if you're not using this on wolfy1339's VPS
 SCREEN1_ID=12509
 SCREEN2_ID=12491
@@ -6,11 +6,9 @@ SCREEN2_ID=12491
 git pull
 
 #Run Node servers in background
-if [-z $SCREEN1_ID] && [-z $SCREEN2_ID]
-    then
-        screen -r  $SCREEN1_ID -X stuff 'node app.js\n'
-        screen -r $SCREEN2_ID -X stuff 'node app_static.js\n'
-    else
-        node app.js &
-        node app_static.js &
-fi
+screen -r  $SCREEN1_ID -X stuff 'node app.js\n'
+screen -r $SCREEN2_ID -X stuff 'node app_static.js\n'
+
+# Uncomment these if you're not using this on wolfy1339's VPS
+#node app.js &
+#node app_static.js &
