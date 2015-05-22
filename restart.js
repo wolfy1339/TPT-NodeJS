73c0b750ab8ef08879b1c239348620beb739e0de
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-05-22 12:46:25 -0400

    Start the restart function

---
+++ restart.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var child;
+var cp = require('child_process');
+var exec = cp.exec;
+var spawn = cp.spawn;
+var port = process.env.PORT || 3011;
+
+function restartApp(req, res, e) {
+    spawn('git', ['pull']);   // git pull
+    spawn('npm', ['install']); // npm install
+    child.kill();
+    startApp(e);
+    res.send('Ok');
+}
+
+function startApp(e) {
+    child = spawn('node', [e]);
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', function (data) {
+        var str = data.toString()
+        console.log(str);
+    });
+    child.on('close', function (code) {
+        console.log('Process exit code %d', code);
+    });
+}
+
+app.post('/', restartApp);
+restartApp();
