f834c14f1c18494dc76fbc1c6147e74c7a8dd4dc
io <io4@users.noreply.github.com>
2015-05-26 19:32:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -749,7 +749,7 @@ app.post('/Save.api', function(req, res) {
                 }
                 console.log('Current ID was updated!');
             });
-             client.say('#BMN', 'A save called ' + sData.Name + ' was uploaded');
+             client.say('#BMNNet', 'A save called ' + sData.Name + ' was uploaded');
             fs.writeFile(path.join(__dirname, 'Saves', 'save_' + sID + '.txt'), '{"ID":' + sID +
                 ',"Favourite":false,"Score":1,"ScoreUp":1,"ScoreDown":0,"Views":1,"ShortName":"' + sData.Name + '","Name":"' + sData.Name +
                 '","Description":"' + sData.Description + '", "DateCreated":0,"Date":0,"Username":"' + sess.TPTuser +
