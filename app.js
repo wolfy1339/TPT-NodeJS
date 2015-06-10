8252758053036740fbe4503942a35dd17be1225f
io <io4@users.noreply.github.com>
2015-06-09 22:54:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -794,17 +794,17 @@ app.post('/Save.api', function(req, res) {
             fs.createReadStream(sData2.Data.path).pipe(fs.createWriteStream(path.join(__dirname, 'Saves_bin', sID + '.cps')));
             fs.createReadStream(sData2.Data.path).pipe(fs.createWriteStream(path.join(__dirname, 'Saves_bin', sID + '_1.cps')));
             //var spawn = require('child_process').spawn;
-            var exec = require('child_process').exec;
+            var exec = require('child_process').execFile;
             var spawn = require('child_process').spawn;
             if (isWindows) {
-                //child = spawn('Render', [sID + '.cps', sID], {
-                child = exec('Render', [sID + '.cps', sID], {
+                //child = exec('Render', [sID + '.cps', sID], {
+                child = spawn('Render', [sID + '.cps', sID], {
                     cwd: path.join(__dirname, 'Saves_bin')
                 });
             } else {
                 if (isX64) {
                    //child = exec('Render', [sID + '.cps', sID], {
-                    child = spawn('./render64', [sID + '.cps', sID], {
+                    child = exec('render64', [sID + '.cps', sID], {
                         cwd: path.join(__dirname, 'Saves_bin')
                     });
                     console.log('./Render '+ sID + '.cps ' + sID);
