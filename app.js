1edfe3261074ebd5ba54271d36c813f10ea70f35
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-08-21 17:47:17 -0400

    Fix last commit + formatting changes

--- app.js
+++ app.js
@@ -250,14 +250,15 @@ app.post('/Browse/Report.json', function(req, res) {
     var request = require('request');
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, Data) {
-        if(ptauth[req.get('X-Auth-User-Id')].Key==req.get('X-Auth-Session-Key')){
-			ReportFile.write(new Date().toSting + ':Report from UserID: '+req.get('X-Auth-User-Id')+". From IP: "+ip+"Report Submited for SaveID: "+req.query.ID+". Report Reason: "+Data.Reason);
+        if(ptauth[req.get('X-Auth-User-Id')].Key == req.get('X-Auth-Session-Key')){
+			ReportFile.write(new Date().toString + ':Report from UserID: ' + req.get('X-Auth-User-Id') + '. From IP: ' + ip + 'Report Submited for SaveID: ' + req.query.ID + '. Report Reason: ' + Data.Reason);
+			client.notice('+##BMNNet', 'New report for save: %d', req.query.ID);
 		} else {
-		req.end("Not logged in. Your IP has been logged, and the admins contacted.");
-        console.warn('Someone not logged in tried to manually report ' + ip);
-        client.notice('+##BMNNet', 'Someone not logged in tried to manually report from '+ip);
+		    req.end("Not logged in. Your IP has been logged, and the admins contacted.");
+            console.warn('Someone not logged in tried to manually report %d', ip);
+            client.notice('+##BMNNet', 'Someone who is not logged in tried to manually report from %d', ip);
 		}
-        });
+    });
 });
 /*
 app.all('/deploy', function(req, res) {
