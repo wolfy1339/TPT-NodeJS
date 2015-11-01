d35972e19f6ade4a6388968564e8ed0f79ecf726
io <io4@users.noreply.github.com>
2015-10-31 21:45:09 -0300

    Fix

--- app.js
+++ app.js
@@ -774,11 +774,6 @@ app.post('/Login.json', function(req, res) {
                                      res.writeHead(200, {
                                      'Content-Type': 'text/json'
                                     });
-                                    ptauth.loginsuccess=true;
-                                     }
-                            });
-                        if(ptauth.loginsuccess){
-                        ptauth.loginsuccess=false;
                         ptauth[dataa[2]] = {};
                         ptauth[dataa[2]].Name = dataa[0];
                         ptauth[dataa[2]].Key = Math.random();
@@ -830,6 +825,7 @@ app.post('/Login.json', function(req, res) {
                         res.end();
                     });
                 }
+                });
             });
         });
     //});
